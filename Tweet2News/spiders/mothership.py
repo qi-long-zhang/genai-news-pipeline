96cbfdb100d46bb3fc535e39bcb097993e09baea
Zhang Qilong <zhangcqypt@icloud.com>
2026-01-27 09:37:46 +0800

    Exclude Instagram blockquotes in Mothership spider

--- Tweet2News/spiders/mothership.py
+++ Tweet2News/spiders/mothership.py
@@ -58,7 +58,9 @@ class MothershipSpider(scrapy.Spider):
         content_section = response.css("div.content")
 
         content = []
-        text_nodes = content_section.css(":scope > h2, :scope > p, :scope > blockquote")
+        text_nodes = content_section.css(
+            ":scope > h2, :scope > p, :scope > blockquote:not(.instagram-media)"
+        )
         for text_node in text_nodes:
             tag = text_node.root.tag
             all_text = text_node.xpath(".//text()[not(ancestor::figure)]").getall()
