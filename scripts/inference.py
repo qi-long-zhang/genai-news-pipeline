143b4269626b38bda4e6c22317253c8aaa5bb149
Zhang Qilong <zhangcqypt@icloud.com>
2026-01-29 19:52:30 +0800

    Update inference.py example format

--- scripts/inference.py
+++ scripts/inference.py
@@ -3,18 +3,8 @@ from gradio_client import Client
 client = Client("zhang-qilong/ModernBERT-News")
 
 payload = [
-    {
-        "text": "Former US Vice President Dick Cheney dies at 84",
-        "text_pair": "He served as Vice-President to 43rd US President George W. Bush.",
-    },
-    {
-        "text": "Man dies on SIA flight bound for Milan, wife tears & thanks doctor for trying to save husband",
-        "text_pair": "Three doctors and almost all the cabin crew attempted CPR.",
-    },
-    {
-        "text": "S'porean girl, 2, dons kebaya to become youngest & cutest SIA stewardess for a day",
-        "text_pair": "Interns are preparing from such a young age for jobs these days.",
-    },
+    "Headline: \nSubhead: \nLead: ",
+    "Headline: Nanyang Optical undergoes voluntary liquidation after 65 years, customer's S$1,800 order stuck in limbo\nSubhead: The company also provided the customer with a number to call but her calls went unanswered.\nLead: A long-time customer of Nanyang Optical has found her S$1,800 contact lens order stuck in limbo, after the company informed customers that it is undergoing voluntary liquidation.",
 ]
 
 result = client.predict(
