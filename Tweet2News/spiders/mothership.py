2373b62877bc93ab1cadb6f504b4418c7e8fd991
Zhang Qilong <zhangcqypt@icloud.com>
2026-02-21 17:55:02 +0800

    Update mothership spider homepage parsing

--- Tweet2News/spiders/mothership.py
+++ Tweet2News/spiders/mothership.py
@@ -1,6 +1,5 @@
-import json
 import scrapy
-from datetime import timedelta, timezone, datetime
+from datetime import timedelta, timezone
 from dateutil import parser
 from pymongo import MongoClient
 from urllib.parse import urljoin
@@ -17,91 +16,87 @@ class MothershipSpider(scrapy.Spider):
         mongo_db = self.settings.get("MONGO_DATABASE")
         mongo_collection = self.name
 
-        self.page = 1
-        self.max_page = 2
-        self.cutoff_date = datetime.now(timezone.utc) - timedelta(days=3)
-        self.existing_articles = {}
-
         with MongoClient(mongo_uri, tz_aware=True) as client:
             collection = client[mongo_db][mongo_collection]
-            cursor = collection.find(
-                {"article.update_date": {"$gte": self.cutoff_date}},
-                projection={
-                    "_id": 1,
-                    "article.update_date": 1,
-                },
+
+            latest_doc = collection.find_one(
+                projection={"article.publish_date": 1},
+                sort=[("article.publish_date", -1)],
             )
-            for doc in cursor:
-                article_data = doc.get("article", {})
-                u_date = article_data.get("update_date")  # UTC
-                if u_date:
-                    self.existing_articles[doc["_id"]] = u_date
+            if latest_doc and (
+                latest_publish_date := latest_doc.get("article", {}).get("publish_date")
+            ):
+                self.cutoff_date = latest_publish_date + timedelta(seconds=1)
 
         yield scrapy.Request(
-            url=f"https://mothership.sg/json/posts-{self.page}.json",
-            headers={"Referer": "https://mothership.sg/"},
+            url="https://mothership.sg/",
             meta={"cloudscraper": True},
         )
 
-    def parse(self, response):
-        def _parse_date(date_str):
-            if not date_str:
-                return None
-            dt = parser.parse(date_str)
-            if dt.tzinfo is None:
-                dt = dt.replace(tzinfo=timezone(timedelta(hours=8)))
-            return dt.astimezone(timezone.utc)
+    @staticmethod
+    def _clean(value):
+        return value.strip() if value else None
 
-        data = json.loads(response.text)
-        if not data:
+    @staticmethod
+    def _parse_date(date_str):
+        if not date_str:
+            return None
+        dt = parser.parse(date_str)
+        if dt.tzinfo is None:
+            dt = dt.replace(tzinfo=timezone(timedelta(hours=8)))
+        return dt.astimezone(timezone.utc)
+
+    def parse(self, response):
+        articles = response.css("article[data-post-id]")
+        if not articles:
             return
 
-        for article in data:
-            date = _parse_date(article.get("date"))  # UTC
-            if date and date < self.cutoff_date:  # UTC compare
+        for article in articles:
+            date = self._parse_date(
+                self._clean(article.css("span.meta-time::text").get())
+            )  # UTC
+            if date and date < self.cutoff_date:
                 return
 
-            article_id = article.get("name")
-            if article_id in self.existing_articles:
-                existing_update_date = self.existing_articles[article_id]  # UTC
-                if date <= existing_update_date:  # UTC compare
-                    continue
+            url = (
+                article.css("h2.post-title a::attr(href)").get()
+                or article.css("div.image-box a::attr(href)").get()
+            )
+            if not url:
+                continue
+
+            _id = self._clean(article.attrib.get("data-post-id"))
+            if not _id:
+                continue
 
             item = NewsArticleItem()
-            item["_id"] = article_id
-            item["url"] = article.get("url")
+            item["_id"] = _id
+            item["url"] = url
             item["source"] = "Mothership"
 
-            item["title"] = article.get("title")
-            item["subtitle"] = article.get("excerpt")
-            item["cover_image"] = article.get("image_url")
+            item["title"] = self._clean(article.css("h2.post-title a::text").get())
+            item["cover_image"] = article.css("div.image-box img::attr(src)").get()
             item["publish_date"] = date  # UTC
             item["update_date"] = date  # UTC
 
             yield scrapy.Request(
-                item["url"],
+                url,
                 self.parse_article,
                 meta={"cloudscraper": True, "item": item},
             )
 
-        next_page = self.page + 1
-        if next_page > self.max_page:
-            return
-        next_url = response.url.replace(
-            f"posts-{self.page}.json", f"posts-{next_page}.json"
-        )
-        self.page = next_page
-        yield scrapy.Request(next_url, self.parse)
-
     def parse_article(self, response):
-        def _clean(value):
-            return value.strip() if value else None
-
         item = response.meta["item"]
 
-        item["author"] = response.css(
-            "div.article-head div.author-time a[href='#author'].underline::text"
-        ).get()
+        item["subtitle"] = self._clean(
+            response.css("div.article-head p.sub-title::text").get()
+        )
+
+        item["author"] = self._clean(
+            response.css(
+                "div.article-head div.author-time a[href='#author'].underline::text"
+            ).get()
+        )
 
         content_section = response.css("div.content")
 
@@ -112,7 +107,7 @@ class MothershipSpider(scrapy.Spider):
         for text_node in text_nodes:
             tag = text_node.root.tag
             all_text = text_node.xpath(".//text()[not(ancestor::figure)]").getall()
-            text = " ".join(cleaned for t in all_text if (cleaned := _clean(t)))
+            text = " ".join(cleaned for t in all_text if (cleaned := self._clean(t)))
             if tag in {"h2", "h3"} and text and "Related" in text:
                 continue
             if text:
@@ -120,7 +115,7 @@ class MothershipSpider(scrapy.Spider):
         item["content"] = content
 
         images = []
-        featured_image_url = _clean(
+        featured_image_url = self._clean(
             response.css("div.image.featured img::attr(src)").get()
         )
         if featured_image_url:
@@ -128,7 +123,7 @@ class MothershipSpider(scrapy.Spider):
 
         image_nodes = content_section.css("figure")
         for img_node in image_nodes:
-            img_url = _clean(img_node.css("img::attr(src)").get())
+            img_url = self._clean(img_node.css("img::attr(src)").get())
             caption = img_node.xpath("normalize-space(.//figcaption)").get()
             if img_url:
                 images.append({"url": img_url, "caption": caption})
@@ -139,7 +134,7 @@ class MothershipSpider(scrapy.Spider):
             "iframe[src*='youtube.com'], iframe[src*='youtu.be']"
         )
         for vid_node in video_nodes:
-            vid_url = _clean(vid_node.css("::attr(src)").get())
+            vid_url = self._clean(vid_node.css("::attr(src)").get())
             if vid_url:
                 videos.append(vid_url)
         item["videos"] = videos
@@ -148,8 +143,8 @@ class MothershipSpider(scrapy.Spider):
         links = []
         link_nodes = content_section.css("a[href]")
         for link_node in link_nodes:
-            link_url = _clean(link_node.css("::attr(href)").get())
-            link_text = _clean(link_node.css("::text").get())
+            link_url = self._clean(link_node.css("::attr(href)").get())
+            link_text = self._clean(link_node.css("::text").get())
             if not link_url or link_url in excluded_links or not link_text:
                 continue
             if "email-protection" in link_url or "[email" in link_text:
@@ -165,7 +160,7 @@ class MothershipSpider(scrapy.Spider):
         embeds = []
         embed_nodes = content_section.css("iframe[src^='/']")
         for embed_node in embed_nodes:
-            embed_url = _clean(embed_node.css("::attr(src)").get())
+            embed_url = self._clean(embed_node.css("::attr(src)").get())
             if embed_url:
                 embeds.append(urljoin(response.url, embed_url))
         item["embeds"] = embeds
